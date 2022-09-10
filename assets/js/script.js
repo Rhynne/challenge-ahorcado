904fc4e16433ce5498a38859ce5bd1748d620a33
Lorenzo Rivero <lrhynne@gmail.com>
2022-09-10 20:27:43 -0300

    10 attempts, gallows is drawn on failure

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ function drawCircle(x, y, radius, color) {
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2 * Math.PI);
   ctx.stroke();
+  ctx.closePath();
 }
 
 function drawText(text, x, y, font, color) {
@@ -34,16 +35,6 @@ function drawText(text, x, y, font, color) {
 
 // Gallows
 drawLine(250, 400, 550, 400, 5, 'round', '#0A3871');
-drawLine(300, 50, 300, 400, 5, 'round', '#0A3871');
-drawLine(300, 50, 500, 50, 5, 'round', '#0A3871');
-drawLine(500, 50, 500, 100, 5, 'round', '#0A3871');
-
-drawCircle(500, 130, 30, '#0A3871');
-drawLine(500, 160, 500, 280, 5, 'round', '#0A3871');
-drawLine(500, 160, 450, 220, 5, 'round', '#0A3871');
-drawLine(500, 160, 550, 220, 5, 'round', '#0A3871');
-drawLine(500, 280, 450, 340, 5, 'round', '#0A3871');
-drawLine(500, 280, 550, 340, 5, 'round', '#0A3871');
 
 // Text
 const words = ['vue', 'react', 'angular'];
@@ -51,7 +42,7 @@ const wordPicked = randomNumber(words.length);
 const wordUpperCase = words[wordPicked].toUpperCase();
 const wordSplit = wordUpperCase.split('');
 let tw = cw / wordSplit.length;
-let x = 210;
+let x = 200;
 
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
@@ -66,22 +57,64 @@ function drawWord() {
 drawWord();
 
 let keys = [];
+let keysDrawed = [];
+let attempt = 0;
 
 document.addEventListener('keydown', (e) => {
   let key = e.key;
   key = key.toUpperCase();
 
-  if (!keys.includes(key) && /^[A-Z]+$/i.test(key)) {
+  if (!keys.includes(key) && isNaN(key)) {
     keys.push(key);
   }
 
-  if (!wordUpperCase.includes(key) && /^[A-Z]+$/i.test(key)) {
-    drawText(key, x + 5 + tw, 530, '24px Inter', '#495057');
+  if (
+    !wordUpperCase.includes(key) &&
+    isNaN(key) &&
+    !keysDrawed.includes(key) &&
+    attempt < 10
+  ) {
+    drawText(key, x + tw, 530, '24px Inter', '#495057');
+    keysDrawed.push(key);
     x = x + 30;
+    attempt = attempt + 1;
+
+    switch (attempt) {
+      case 1:
+        drawLine(300, 50, 300, 400, 5, 'round', '#0A3871');
+        break;
+      case 2:
+        drawLine(300, 50, 500, 50, 5, 'round', '#0A3871');
+        break;
+      case 3:
+        drawLine(500, 50, 500, 100, 5, 'round', '#0A3871');
+        break;
+      case 4:
+        drawCircle(500, 130, 30, '#0A3871');
+        break;
+      case 5:
+        drawLine(500, 160, 500, 280, 5, 'round', '#0A3871');
+        break;
+      case 6:
+        drawLine(500, 160, 450, 220, 5, 'round', '#0A3871');
+        break;
+      case 7:
+        drawLine(500, 160, 550, 220, 5, 'round', '#0A3871');
+        break;
+      case 8:
+        drawLine(500, 280, 450, 340, 5, 'round', '#0A3871');
+        break;
+      case 9:
+        drawLine(500, 280, 550, 340, 5, 'round', '#0A3871');
+        break;
+      default:
+        break;
+    }
   } else {
     for (let i = 0; i < wordSplit.length; i++) {
-      if (wordSplit[i].includes(key)) {
+      if (wordSplit[i].includes(key) && !keysDrawed.includes(key)) {
         drawText(key, 255 + tw * i, 480, '48px Inter', '#0A3871');
+        keysDrawed.push(key);
       }
     }
   }
